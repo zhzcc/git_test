568f327f9ce005fae2c6edf421729ba6402fd704
zhanghuazhi <zhzcc99@163.com>
2023-07-23 21:28:10 +0800

    初始化

---
+++ src/main/java/com/example/demo_github/DemoGithubApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_github;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGithubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoGithubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demo_github/DemoGithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_github;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGithubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
